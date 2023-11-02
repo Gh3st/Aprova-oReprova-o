fe7add25ff5c587cd95fb56a854a076940187c33
natin <natin@OVVQX507>
2023-11-01 21:24:47 -0300

    Adicionar arquivos de projeto.

---
+++ MédiaDosAlunos/a hora que o filho chora e a mãe não ver.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Informe 3 notas do aluno separadamente para calcularmos a média ex: primeira nota enter, segunda nota enter, terceira nota enter");
+int one = int.Parse(Console.ReadLine());
+int two = int.Parse(Console.ReadLine());
+int three = int.Parse(Console.ReadLine());
+    
+int média = (one + two + three)/3;
+
+if(média >= 7)
+{
+    Console.WriteLine($"Parabéns seu filho(a) tirou {média} está aprovado(a) :)");
+}
+else
+{
+    Console.WriteLine($"É uma pena mas seu filho(a) tirou {média} na média final então ele foi reprovado(a) :(");
+}
